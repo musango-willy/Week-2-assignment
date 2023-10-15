fb5c0d069427eb4ec255e0b312ec16aa670d1d8d
musango willy <musangowilly@gmail.com>
2023-10-15 23:20:08 +0300

    added

--- week3assignment.js
+++ week3assignment.js
@@ -9,5 +9,12 @@
 const myVaue = 86;
 console.log(myVaue);
 
+
+//qn 4
 let FirstName="musango";
 console.log(FirstName);
+let LastName="willy";
+console.log(LastName);
+let GuruCodingCourse="JavaScript";
+console.log(GuruCodingCourse);
+
