5c3b7a9216b826383d6fd5a7734856c140653b0d
musango willy <musangowilly@gmail.com>
2023-10-16 00:06:38 +0300

    number 5

--- week3assignment.js
+++ week3assignment.js
@@ -21,9 +21,15 @@ let TestScore="100%";
 console.log(TestScore);
 
 //qn 5
+const averageJavaScriptscore="86";
 
 //qn 6
 
 for(let count=1;count<=20;count++){
     console.log(count);
 }
+ //for loop iterates to check spcefied conditions and then repeatedly executes a block of code as long as those conditions are met.
+ //declared a variable named "count" and initilized it 
+ // count<=20 it is the conditions that the for loop must evaluate and met
+ //count++ it increments the value of count +1 after each iteration until it meets the conditions
+ //console.log(count) prints the output of the loop after executing the block of code
